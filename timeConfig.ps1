c237c57b7596b1d588aa670844af04284517622c
Danny Murphy <6512845+dltmurphy@users.noreply.github.com>
2020-11-24 10:24:59 +0000

    Update timeConfig.ps1

--- timeConfig.ps1
+++ timeConfig.ps1
@@ -1,2 +1,2 @@
-Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "NtpServer" -PropertyType "String" -Value "time.cloudflare.com,0x9" -Force
-Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "Type" -PropertyType "String" -Value "NTP" -Force
+Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "NtpServer" -Type "String" -Value "time.cloudflare.com,0x9" -Force
+Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "Type" -Type "String" -Value "NTP" -Force
