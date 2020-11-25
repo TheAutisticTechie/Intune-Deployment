70b55ed723065e61a488eea4bb522c90504d0bf8
Danny Murphy <6512845+dltmurphy@users.noreply.github.com>
2020-11-25 10:37:19 +0000

    Update timeConfig.ps1

--- timeConfig.ps1
+++ timeConfig.ps1
@@ -1,2 +1,17 @@
-Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "NtpServer" -Type "String" -Value "time.cloudflare.com,0x9" -Force
-Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "Type" -Type "String" -Value "NTP" -Force
+$NtpServer = (Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters\).NtpServer
+$type = (Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters\).Type
+$service = get-service -name "Windows Time"
+
+if ($NtpServer -ne "time.cloudflare.com,0x9") {
+    Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "NtpServer" -Type "String" -Value "time.cloudflare.com,0x9" -Force
+}
+
+if ($type -ne "NTP") {
+    Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "Type" -Type "String" -Value "NTP" -Force
+}
+
+if ($service.Status -ne "Running") {
+    Start-Service $service
+}
+
+w32tm /resync
