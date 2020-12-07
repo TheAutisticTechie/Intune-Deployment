d8b4afad1f574f1f5f501abade652c2072dbeca4
Danny Murphy <6512845+dltmurphy@users.noreply.github.com>
2020-12-07 15:45:44 +0000

    Update timeConfig.ps1

--- timeConfig.ps1
+++ timeConfig.ps1
@@ -2,12 +2,18 @@ $NtpServer = (Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\W32
 $type = (Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters\).Type
 $service = get-service -name "Windows Time"
 
+w32tm /config /manualpeerlist:time.cloudflare.com,0x9 /syncfromflags:MANUAL /update
+
 if ($NtpServer -ne "time.cloudflare.com,0x9") {
     Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "NtpServer" -Type "String" -Value "time.cloudflare.com,0x9" -Force
 }
 
 if ($type -ne "NTP") {
     Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "Type" -Type "String" -Value "NTP" -Force
+}
+
+if ($service.StartType -ne "Automatic" {
+    Set-Service w32time -StartupType "Automatic"
 }
 
 if ($service.Status -ne "Running") {
