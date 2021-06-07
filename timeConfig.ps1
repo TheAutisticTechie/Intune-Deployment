ce3a0324cc51fb43e397a659772848e30ad8e958
Danny Murphy <6512845+dltmurphy@users.noreply.github.com>
2021-06-07 21:36:24 +0100

    Update timeConfig.ps1

--- timeConfig.ps1
+++ timeConfig.ps1
@@ -1,23 +1,57 @@
+# Stop if error
+$ErrorActionPreference = "stop"
+
+# Variables
 $NtpServer = (Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters\).NtpServer
 $type = (Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters\).Type
 $service = get-service -name "Windows Time"
 
-w32tm /config /manualpeerlist:"time.cloudflare.com,0x4" /syncfromflags:MANUAL /update
-
-if ($NtpServer -ne "time.cloudflare.com,0x4") {
-    Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "NtpServer" -Type "String" -Value "time.cloudflare.com,0x4" -Force
+try {
+    w32tm /config /manualpeerlist:"time.cloudflare.com,0x4" /syncfromflags:MANUAL /update
+    try {
+        if ($NtpServer -ne "time.cloudflare.com,0x4") {
+            Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "NtpServer" -Type "String" -Value "time.cloudflare.com,0x4" -Force
+            return
+        }
+    }
+    catch [System.Management.Automation.PSArgumentException] {
+        Write-Host "Unable to write to registry"
+    }
+    try {
+        if ($type -ne "NTP") {
+            Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "Type" -Type "String" -Value "NTP" -Force
+            return
+        }
+    }
+    catch [System.Management.Automation.PSArgumentException] {
+        Write-Host "Unable to write to registry"
+    }
+    try {
+        if ($service.StartType -ne "Automatic" ) {
+            Set-Service w32time -StartupType "Automatic"
+            return
+        }
+    }
+    catch [SystemException] {
+        Write-Host "Error processing Windows Time service."
+    }
+    try {
+        if ($service.Status -ne "Running") {
+            Start-Service "Windows Time"
+            return
+        } else {
+            Restart-Service "Windows Time"
+            return
+        }
+    }
+    catch [SystemException] {
+        write-host "Error processing Windows Time service."
+    }
 }
-
-if ($type -ne "NTP") {
-    Set-ItemProperty -Path Registry::"HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name "Type" -Type "String" -Value "NTP" -Force
+catch {
+    Write-Host "An error has occurred"
 }
-
-if ($service.StartType -ne "Automatic" {
-    Set-Service w32time -StartupType "Automatic"
+finally {
+    $ErrorActionPreference = "continue"
+    w32tm /resync
 }
-
-if ($service.Status -ne "Running") {
-    Start-Service "Windows Time"
-}
-
-w32tm /resync
