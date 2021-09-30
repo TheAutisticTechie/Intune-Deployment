9e1d55feaad927e8ab6e1a4a4fd0dce4605fb3e2
Danny Murphy <6512845+dltmurphy@users.noreply.github.com>
2021-09-30 13:26:04 +0100

    update name and <#

--- WindowsUpdateReset.ps1
+++ WindowsUpdateReset.ps1
@@ -1,3 +1,31 @@
+<#
+.SYNOPSIS
+    Reset Windows Update Services to Microsoft's CDN
+.DESCRIPTION
+    (c) Danny Murphy. All rights reserved.
+    Script provided as-is without any warranty of any kind. Use it freely at your own risks.
+    Must be run with elevated permissions. 
+    Designed to be run as a SYSTEM PowerShell Script from Intune
+    The script will reset WSUS to Microsoft
+    Requires PowerShell 3.0.
+.INPUTS
+  None
+.OUTPUTS
+  None
+.NOTES
+  Version:          2.0
+  Author:           Danny Murphy
+  Twitter:          @dltmurphy
+  Creation Date:    19 September 2021
+  Purpose/Change:   Reset Windows Update Services to Microsoft's CDN
+.EXAMPLE
+  .\windowsUpdateReset.ps1
+  Run as an administator or SYSTEM
+#>
+
+#Requires -Version 3
+#Requires -Runasadministrator
+
 $reg = "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate"
 
 if (Test-Path $reg) {
--- iisBinding.ps1
+++ iisBinding.ps1
@@ -2,13 +2,13 @@ $thumb = Read-Host "Enter the certificate thumbprint"
 $cert = Get-ChildItem Cert:\LocalMachine\WebHosting |Where-Object {$_.Thumbprint -eq $thumb}
 
 $siteAHttpHost = @(
-    #"example.com",
-    #"www.example.com"
+    "example.com",
+    "www.example.com"
 )
 
 $siteAHttpsHost = @(
-    #"example.com",
-    #"www.example.com"
+    "example.com",
+    "www.example.com"
 )
 
 $siteA = Read-Host "Enter the Site Name"
@@ -24,4 +24,4 @@ foreach ($hostname in $siteAHttpsHost){
     New-Item -Path "IIS:\SslBindings\!443!$hostname" -Value $cert -SSLFlags 1
 }
 
-#Expand as necessary
+# Expand as necessary
