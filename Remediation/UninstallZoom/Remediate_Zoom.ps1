7e10675c2422b88dba1939e3a76b59ab055ae8f2
Danny Chrismas <danny+github@chrismas.co.uk>
2023-07-18 11:18:56 +0100

    Update Remediate_Zoom.ps1

--- Remediation/UninstallZoom/Remediate_Zoom.ps1
+++ Remediation/UninstallZoom/Remediate_Zoom.ps1
@@ -12,9 +12,20 @@ $regKey = test-path -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Unins
 
 try {
     if(($regKey) -or ($appdataTest)){
+        $process = get-process zoom -ErrorAction SilentlyContinue
         write-host "Detected, attempting uninstall"
-        & $env:APPDATA\Zoom\uninstall\installer.exe /uninstall
-        exit 0 # detected
+        if (!$process){
+            $job = Start-Job { & $env:APPDATA\Zoom\uninstall\installer.exe /uninstall /silent }
+            Wait-Job $job
+            Receive-Job $job
+            Start-Sleep -Seconds 30#uninstaller ends writing to the log at different speeds
+            remove-item $env:APPDATA\Zoom\ -Force -Recurse
+            exit 0 # detected
+        }
+        else {
+            write-host "Error: meeting in progress. Ending Uninstall attempt"
+            exit 1
+        }
     } else {
         Write-host "Not Detected"
         exit 1 # doesn't exist
